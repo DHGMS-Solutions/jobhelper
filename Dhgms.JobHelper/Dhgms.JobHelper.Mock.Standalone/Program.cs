91749619a61239e4098e51441a7d26a201a08700
David Vreony <dpvreony@hotmail.com>
2015-03-26 22:38:21 +0000

    addition of mock placeholder projects

---
+++ Dhgms.JobHelper/Dhgms.JobHelper.Mock.Standalone/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dhgms.JobHelper.Mock.Standalone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
